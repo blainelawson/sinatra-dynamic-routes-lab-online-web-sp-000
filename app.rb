0c5db9a1293195e42ad611b9ae6a915f8d73b61f
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-14 13:38:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
   end
   # binding.pry
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{word1} " + "#{word2} " + "#{word3} " + "#{word4} " + "#{word5} "
+    "#{word1} " + "#{word2} " + "#{word3} " + "#{word4} " + "#{word5}."
   end
 
 end
